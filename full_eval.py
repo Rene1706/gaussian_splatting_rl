f07b9fcbbbdbf24a1b73dda65fef0c83c572b2b9
Rene Huertgen <rene.huertgen@gmx.de>
2024-10-19 17:18:46 +0200

    Added render and metric gathering in evaluation
    Signed-off-by: Rene Huertgen <rene.huertgen@gmx.de>

--- full_eval.py
+++ full_eval.py
@@ -9,6 +9,7 @@ import datetime
 import pprint
 from pathlib import Path
 import random
+import shutil
 
 # Get the current script directory
 script_dir = Path(__file__).parent
@@ -70,12 +71,45 @@ def run_command(command, env=None, timeout=7200):  # Timeout set to 2 hours (720
         print(stderr, file=sys.stderr)
         raise RuntimeError(f"Command failed with return code {process.returncode}: {command}")
 
+def get_latest_output_folder(output_base_path):
+    subfolders = [os.path.join(output_base_path, d) for d in os.listdir(output_base_path) if os.path.isdir(os.path.join(output_base_path, d))]
+    if not subfolders:
+        return None
+    latest_subfolder = max(subfolders, key=os.path.getmtime)
+    return latest_subfolder
+
+def delete_train_test_folders(numbered_folder):
+    """Deletes 'train' and 'test' folders inside the numbered folder."""
+    train_folder = os.path.join(numbered_folder, 'train')
+    test_folder = os.path.join(numbered_folder, 'test')
+    
+    # Remove 'train' folder if it exists
+    if os.path.exists(train_folder) and os.path.isdir(train_folder):
+        shutil.rmtree(train_folder)
+        print(f"Deleted {train_folder}")
+    
+    # Remove 'test' folder if it exists
+    if os.path.exists(test_folder) and os.path.isdir(test_folder):
+        shutil.rmtree(test_folder)
+        print(f"Deleted {test_folder}")
+
+def rename_metrics_output(output_folder, dataset):
+    files_to_rename = ["gaussian_num_points.txt", "per_view.json", "results.json"]
+    for file in files_to_rename:
+        os.rename(os.path.join(output_folder, file), os.path.join(output_folder, f"{file.split('.')[0]}_{dataset}.{file.split('.')[1]}"))
+
+def get_training_config(meta_model_path):
+    override_path = os.path.dirname(meta_model_path)
+    overrides_file = os.path.join(override_path, '.hydra', 'overrides.yaml')
+    destination_file = os.path.join(os.getcwd(), 'overrides.yaml')
+    shutil.copy(overrides_file, destination_file)
+
 def train_and_evaluate(cfg, datasets, output_path):
 
     # Hardcoded training and evaluation datasets
     training_datasets = [
         "03Mallard", "05Whale", "07Owl", "09Swan",
-        "11Pig", "13Phoenix", "15Parrot", "17Scorpion", "02Unicorn", "04Turtle", "06Bird", "08Sabertooth", "10Sheep",
+        "11Pig", "13Pheonix", "15Parrot", "17Scorpion", "02Unicorn", "04Turtle", "06Bird", "08Sabertooth", "10Sheep",
         "12Zalika", "14Elephant", "16Cat"
     ]
     evaluation_datasets = [
@@ -92,23 +126,24 @@ def train_and_evaluate(cfg, datasets, output_path):
 
     full_data_path = os.path.abspath(full_eval_output_path)
 
-    for epoch in range(cfg.eval_params.epochs):
-        print(f"Running epoch {epoch + 1}/{cfg.eval_params.epochs}")
-        # Sample random datasets for training and evaluation
-        train_dataset = random.choice(training_datasets)
-        print(f"Training on dataset {train_dataset}")
-        # RL Training
-        if not cfg.eval_params.skip_training:
-            cfg.model_params.source_path = os.path.join(script_dir, cfg.eval_params.data_path, train_dataset)
-            #cfg.model_params.source_path = os.path.join(script_dir, cfg.eval_params.data_path, "01Gorilla")
-            cfg.wandb_params.name = f"Final_train_{unique_str}"
-            cfg.wandb_params.id = f"Final_train_{unique_str}"
-            cfg.wandb_params.group = "final_runs"
-            cfg.wandb_params.tags = ["training", "default_pruning", "reinforce",f"reward_{cfg.rl_params.reward_function}", f"lr{str(cfg.rl_params.rl_lr).replace('.', '_')}"]
-            # Optimizing the RL model
-            cfg.rl_params.train_rl = True
-            training_command = create_training_command(cfg)
-            run_command(training_command)
+    if not cfg.eval_params.skip_training:
+        for epoch in range(cfg.eval_params.epochs):
+            print(f"Running epoch {epoch + 1}/{cfg.eval_params.epochs}")
+            # Sample random datasets for training and evaluation
+            train_dataset = random.choice(training_datasets)
+            print(f"Training on dataset {train_dataset}")
+            # RL Training
+            if not cfg.eval_params.skip_training:
+                cfg.model_params.source_path = os.path.join(script_dir, cfg.eval_params.data_path, train_dataset)
+                #cfg.model_params.source_path = os.path.join(script_dir, cfg.eval_params.data_path, "01Gorilla")
+                cfg.wandb_params.name = f"Final_train_{unique_str}"
+                cfg.wandb_params.id = f"Final_train_{unique_str}"
+                cfg.wandb_params.group = "final_runs"
+                cfg.wandb_params.tags = ["training", "default_pruning", "reinforce",f"reward_{cfg.rl_params.reward_function}", f"lr{str(cfg.rl_params.rl_lr).replace('.', '_')}"]
+                # Optimizing the RL model
+                cfg.rl_params.train_rl = True
+                training_command = create_training_command(cfg)
+                run_command(training_command)
 
     if not cfg.eval_params.skip_eval:
         for eval_dataset in evaluation_datasets:
@@ -122,7 +157,26 @@ def train_and_evaluate(cfg, datasets, output_path):
             # Skip optimizing the RL model
             cfg.rl_params.train_rl = False
             training_command = create_training_command(cfg)
+            print(training_command)
             run_command(training_command)
+            get_training_config(cfg.rl_params.meta_model)
+
+            # After training_command, run render.py and metrics.py, then delete train_test folders
+            output_folder = get_latest_output_folder("output")
+            if output_folder:
+                # Run render.py
+                render_command = f"python {script_dir}/render.py -m \"{output_folder}\""
+                print(render_command)
+                run_command(render_command)
+                # Run metrics.py
+                metrics_command = f"python {script_dir}/metrics.py -m \"{output_folder}\""
+                print(render_command)
+                run_command(metrics_command)
+                # Delete train and test folders
+                delete_train_test_folders(output_folder)
+                rename_metrics_output(output_folder, eval_dataset)
+            else:
+                print("No output folder found to process.")
 
 def add_eval_tags(cfg):
     # Add tags for evaluation
